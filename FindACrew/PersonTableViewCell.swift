522b89ddbb9face7a2e5e7e714bd2a206bc77099
Marc Jacques <Marcjacques@me.com>
2019-11-13 20:18:58 -0500

    TableVC setup

---
+++ FindACrew/PersonTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PersonTableViewCell.swift
+//  FindACrew
+//
+//  Created by Marc Jacques on 11/13/19.
+//  Copyright © 2019 Marc Jacques. All rights reserved.
+//
+
+import UIKit
+
+class PersonTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
